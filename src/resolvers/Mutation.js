bdb444638cd857650dd6057f27a85b989da6580c
Jesus Lizama <dev@hellojeb.us>
2019-02-27 11:28:30 -0800

    include jwt and bycrypt libs

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { APP_SECRET, getUserId } = require('../utils');
 
 async function signup(parent, args, context, info) {
